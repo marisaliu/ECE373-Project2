2b85b28d1d9297bb2c813388da8286747a428435
dylanb271 <dylanb271@gmail.com>
2018-10-10 16:38:00 -0400

    outline for gofish

--- gofish.c
+++ gofish.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <deck.h>
+#include <player.h>
 
 int main(int args, char* argv[]) 
 {
+//Start game
+//Shuffle
+//Deal cards
+
+//Player 1 starts
+//Display player 1's hand
+//Display player 1's book
+//Display player 2's book
+
+//Prompt player 1 to enter a rank
+
+//Check player 2's hand to see if they have that rank
+//transfer card function
+
+//while loop for while player 1 is getting cards, prompt for another turn
+//if false, player 1 fishes
+//each time get a card, check for book
+
+//player 2's turn -> goes through same cycle, want generic loop
+
+//keep looping until 7 books
+
+//at end output winner and scores(num of books)
+//ask if want to play again, if yes, start loop again, if no, exit
   fprintf(stdout, "Put your code here.");
 }
