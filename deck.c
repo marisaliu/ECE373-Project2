9a16197b1faa5f91d8453253351116b7c6ef3113
dylanb271 <dylanb271@gmail.com>
2018-10-12 15:02:56 -0400

    cleaned up deck.c

--- deck.c
+++ deck.c
@@ -13,34 +13,17 @@ int shuffle() {
  	newCard.suit = suit[i%4];
 	newCard.rank = rank[i%13];
 	deck_instance.list[i] = newCard;
-  //printf("Card has suit: %c and rank: %c\n", deck_instance.list[i].suit, deck_instance.list[i].rank);
   }
   for(i=TOTALDECK-1; i>=0; i--) {
 	 random = (rand() % (i+1));
-//	 printf("%d\n", random);
 	 newCard = deck_instance.list[i];
 	 deck_instance.list[i] = deck_instance.list[random];
 	 deck_instance.list[random] = newCard;
 	}
   deck_instance.top_card = 0; //cards in deck-1
-/*	for(i=0; i<52; i++){
-printf("Random card has suit: %c and rank: %c\n", deck_instance.list[i].suit, deck_instance.list[i].rank);
-	}
-*/
+
 }
 
-/*int deal_player_cards(struct player* target) {
-  struct card newCard;
-  struct hand currentHand, newHand;
-  int i;
-  target->card_list = &currentHand;
-  for(i=0; i<7; i++) {
-	 newCard = deck_instance.list[deck_instance.top_card++];
-	 currentHand.top = newCard;	
-	 currentHand.next = &newHand;
-	 currentHand = *currentHand.next;
-  }
-}*/
 struct hand* initHand() {
   struct hand *newHand;
   newHand = (struct hand*)malloc(sizeof(struct hand));
@@ -48,7 +31,6 @@ struct hand* initHand() {
 }
 
 int deal_player_cards(struct player* target) {
-  struct card newCard;
   struct hand *currentHand, *newHand;
   int i;
   currentHand = initHand();
@@ -56,8 +38,7 @@ int deal_player_cards(struct player* target) {
 	 newHand = initHand();
 	 if(newHand == NULL) {return -1;}
 	 else{
-	 newCard = deck_instance.list[deck_instance.top_card++];
-	 currentHand->top = newCard;
+	 currentHand->top = deck_instance.list[deck_instance.top_card++];
 	 currentHand->next = newHand;
 	 newHand = currentHand;
 	 }
@@ -74,7 +55,3 @@ size_t deck_size() {
   size_t deckSize = (TOTALDECK - deck_instance.top_card) + 1;
   return deckSize;
 }
-/*
-int main(){
-  shuffle();
-}*/
